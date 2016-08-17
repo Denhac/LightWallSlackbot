15da687d1f7671306498f3d71f8fc38f9d1f90fd
Mark Hoopes <mark.hoopes@aspectsecurity.com>
2016-08-16 21:31:35 -0600

    LightBot Rev 2
    Moved light cycling loop into main parse function.  Light commands no longer block command input, but light commands have an forced 1 second delay in then.
    
    Also added two commands.  Stop halts the current function.  Rainbow produces a chasing rainbow effect but it needs work.

--- lightbot.py
+++ lightbot.py
@@ -12,33 +12,48 @@ slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
+
+current_light_function = None
+current_time_remaining = 0
+    
+numLEDs = 251
+frequency = 1
+#Connect to light controller  
+client = opc.Client('10.0.100.6:80')
 
 def red_alert():
-    numLEDs = 251
-    frequency = 0.5
-    #Connect to light controller  
-    client = opc.Client('10.0.100.6:80')
-
-    for c in range(60): 
-        # [ (red, green, blue) ]
-        pixels = [ (255,0,0) ] * numLEDs #creates an array of all the pixels (the whole row)
-        client.put_pixels(pixels)
-        time.sleep(frequency)
-        pixels = [ (0,0,0) ] * numLEDs 
-        client.put_pixels(pixels)
-        time.sleep(frequency)
+    # [ (red, green, blue) ]
+    pixels = [ (255,0,0) ] * numLEDs #creates an array of all the pixels (the whole row)
+    client.put_pixels(pixels)
+    time.sleep(frequency)
+    pixels = [ (0,0,0) ] * numLEDs 
+    client.put_pixels(pixels)
+    #time.sleep(frequency)
     
+def rainbow():
+    pixels = [ (0,0,0) ] * numLEDs
+    for i in range(numLEDs):
+	pixels[i] = ((80+i+current_time_remaining*10) % 255,
+		     (160+i+current_time_remaining*10) % 255,
+		     (240+i+current_time_remaining*10) % 255)
+    client.put_pixels(pixels)
+	
 
 def handle_command(command, channel):
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    print("Handling")
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-    if command.startswith("red alert"):
+    global current_light_function, current_time_remaining
+    response = "Not sure what you mean. "
+    if command.upper().startswith("STOP"):
+	current_light_function = None
+	current_time_remaining = 0
+	response = "Halting Functions"
+    if command.upper().startswith("RED ALERT"):
+	current_light_function = red_alert
+	current_time_remaining = 10
 	response = "Initiating Red Alert!"
-	red_alert()
+    if command.upper().startswith("RAINBOW"):
+	current_light_function = rainbow
+	current_time_remaining = 30
+	response = "Taste the rainbow"
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
 
@@ -54,9 +69,6 @@ def parse_slack_output(slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
 	    print (output)
-	    if output and 'text' in output:
-		if AT_BOT in output['text']:
-			print ("FOUND IT")
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
@@ -69,6 +81,9 @@ if __name__ == "__main__":
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
         while True:
+	    if (current_time_remaining > 0):
+	        current_light_function()
+		current_time_remaining = current_time_remaining - 1
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
                 handle_command(command, channel)
